9f88259bf37c03878e3b7cd5370e6401907badfa
박승서 <88551771+qkrtmdtj04@users.noreply.github.com>
2023-03-30 22:28:02 +0900

    [Bronze V] Title: 사분면 고르기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.py
@@ -1,11 +1,11 @@
-a = int(input())
-b = int(input())
+x = int(input())
+y = int(input())
 
-if a > 0 and b > 0:
-    print(1)
-elif a > 0 and b < 0:
-    print(4)
-elif a < 0 and b > 0:
-    print(2)
-elif a < 0 and b < 0:
-    print(3)
+if x>0 and y>0:
+  print(1)
+elif x<0 and y>0:
+  print(2)
+elif x<0 and y<0:
+  print(3)
+elif x>0 and y<0:
+  print(4)
